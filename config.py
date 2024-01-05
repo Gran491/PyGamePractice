551d1064dbe51770c79a396836c954283da1d07c
Gran491 <150198902+Gran491@users.noreply.github.com>
2024-01-05 09:17:32 -0700

    updating main, config and sprites

--- config.py
+++ config.py
@@ -1,9 +1,33 @@
-WIN_WIDTH = 640
-WIN_HEIGHT = 480
-TILE_SIZE = 32
+WIN_WIDTH = 1200
+WIN_HEIGHT = 900
+TILE_SIZE = 60
 FPS = 60
 
-PLAYER_LAYER = 1
-
+GROUND_LAYER = 1
+BLOCK_LAYER = 2
+PLAYER_LAYER = 4
+ENEMY_LAYER = 3
+PLAYER_SPEED = 3
+ENEMY_SPEED = 4
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..................B',
+    'B..................B',
+    'B...........BBBBBBBB',
+    'BBBBBBB..E..B......B',
+    'B......B..BBB..E...B',
+    'B......B.......B...B',
+    'B......B.......B...B',
+    'B......B...P...B...B',
+    'B..E...BBBBBBBBB.E.B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+]
--- main.py
+++ main.py
@@ -8,8 +8,26 @@ class Game:
         pygame.init()
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         self.clock = pygame.time.Clock()
-        self.font = pygame.font.SysFont('Arial', 32)
         self.running = True
+        self.font = pygame.font.SysFont('04B_30__.ttf', 32)
+
+        self.character_spritesheet = Spritesheet('img/male_sprites.png')
+        self.terrain_spritesheet = Spritesheet('img/terrain.png')
+        self.enemy_spritesheet = Spritesheet('img/skeleton_sprites.png')
+        self.intro_background = pygame.image.load('img/introbackground.png')
+        self.go_background = pygame.image.load('img/gameover.png')
+        self.attack_spritesheet = Spritesheet('img/attack.png')
+
+    def createTilemap(self):
+        for i, row in enumerate(tilemap):
+            for j, col in enumerate(row):
+                Ground(self, j, i)
+                if col == 'B':
+                    Block(self, j, i)
+                if col == 'E':
+                    Enemy(self, j, i)
+                if col == 'P':
+                    self.player = Player(self, j, i)
 
     def new(self):
         #### a new game starts
@@ -19,7 +37,7 @@ class Game:
         self.enemies = pygame.sprite.LayeredUpdates()
         self.attacks = pygame.sprite.LayeredUpdates()
 
-        self.player = Player(self, 1, 2)
+        self.createTilemap()
 
     def events(self):
         #### game loop events
@@ -27,6 +45,16 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.player.facing == "up":
+                        Attack(self, self.player.rect.x, self.player.rect.y - TILE_SIZE)
+                    if self.player.facing == "down":
+                        Attack(self, self.player.rect.x, self.player.rect.y + TILE_SIZE)
+                    if self.player.facing == "left":
+                        Attack(self, self.player.rect.x - TILE_SIZE, self.player.rect.y)
+                    if self.player.facing == "right":
+                        Attack(self, self.player.rect.x + TILE_SIZE, self.player.rect.y)
 
     def update(self):
         #### game loop updates
@@ -44,13 +72,60 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        self.running = False
 
     def game_over(self):
-        pass
+        text = self.font.render('Game Over', True, WHITE)
+        text_rect = text.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))
+
+        restart_button = Button(10, WIN_HEIGHT - 60, 185, 50, WHITE, BLACK, 'Restart', 32)
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+
+            self.screen.blit(self.go_background, (0,0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
     def intro_screen(self):
-        pass
+        intro = True
+
+        title = self.font.render('Dungeon Crawler', True, BLACK)
+        title_rect = title.get_rect(center=(WIN_WIDTH/2, WIN_HEIGHT/2))
+
+        play_button = Button(535, 500, 125, 50, WHITE, BLACK, 'Play', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
 
 g = Game()
 g.intro_screen()
@@ -61,4 +136,3 @@ while g.running:
 
 pygame.quit()
 sys.exit()
-##### test commit
